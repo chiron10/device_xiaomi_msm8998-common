5cd2460aa5b2845f42e23378ef8a7a4a03a764ef
Ganesh Varma <ganeshvarmai4u@gmail.com>
2020-01-19 15:43:50 +0530

    msm8998: Do not enforce RRO targets

--- msm8998.mk
+++ msm8998.mk
@@ -23,9 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
