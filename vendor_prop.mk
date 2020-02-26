6cee3e7c91f70751ee7d98f0396d6c88a25d5254
andr68rus <ashtmb@gmail.com>
2020-02-26 11:13:45 +0300

    msm8998-common: try to force enable vowifi

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -150,7 +150,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.vendor.qti.telephony.vt_cam_interface=1
+    persist.vendor.qti.telephony.vt_cam_interface=1 \
+    persist.dbg.wfc_avail_ovr=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
